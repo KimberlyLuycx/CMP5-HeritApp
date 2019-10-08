844d4294badc8b968c6f8604add0fa9f2208190c
Kimberly Luycx <kimberly.luycx@student.ap.be>
2019-10-08 16:12:23 +0200

    Views aanmaken

--- app.js
+++ app.js
@@ -3,4 +3,24 @@ const app = express();
 const port = 3000;
 const path = require("path");
 
+// applicatie: zet instellings 'views' op 'mapje views'
+app.set("views", path.resolve(__dirname, "views"));
+
+app.set("view engine", "ejs");
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+// wat achter de slash staat is url die je ingeeft in browse
+app.get('/tweede', function (req, res) {
+  res.render('page2');
+});
+
+app.get('/derde', function (req, res) {
+  res.render('page3');
+});
+
 app.listen(port);
